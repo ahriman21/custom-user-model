d968ee546dc7b4ada2ad061efcef990674d7d89e
ahriman <109135045+ahriman21@users.noreply.github.com>
2023-02-02 21:02:31 +0330

    Update admin_forms.py

--- admin_forms.py
+++ admin_forms.py
@@ -1,34 +1,44 @@
-"""
-User admin panel
-"""
-from django.contrib import admin
+""" This module includes user forms"""
+from django import forms
 from .models import User
-from .forms import UserChangeForm,UserCreationForm
-
-class UserAdmin(admin.ModelAdmin):
-    form = UserChangeForm
-    add_form = UserCreationForm
-
-    list_display = ('email', 'full_name', 'is_staff')
-    list_filter = ('is_staff',)
-    search_fields = ('full_name','email','city')
-    readonly_fields = ('last_login','join_date')
-
-    fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Personal info', {'fields': ('full_name','city')}),
-        ('Permissions', {'fields': ('is_staff',)}),
-    )
-
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'full_name', 'password1', 'password2'),
-        }),
-    )
-
-    ordering = ('email',)
-    filter_horizontal = ('groups','user_permissions')
+from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
 
-admin.site.register(User,UserAdmin)
+class UserCreationForm(forms.ModelForm):
+    """A form for creating new users. Includes all the required
+    fields, plus a repeated password."""
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('email','full_name')
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    disabled password hash display field.
+    """
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = '__all__'
